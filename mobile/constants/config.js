657196afef4f0d323c53b83565b529edb1087870
buikhueanh <anhbui_2026@depauw.edu>
2026-05-07 19:20:44 -0400

    commit

--- mobile/constants/config.js
+++ mobile/constants/config.js
@@ -1,2 +1,12 @@
 export const APP_NAME = "Type A Buddy";
-export const API_BASE_URL = "http://127.0.0.1:8000";
+
+const envApiBaseUrl =
+	(typeof process !== "undefined" &&
+		process.env &&
+		(process.env.EXPO_PUBLIC_API_BASE_URL || process.env.API_BASE_URL)) ||
+	"";
+
+export const API_BASE_URL = (envApiBaseUrl || "http://127.0.0.1:8000").replace(
+	/\/$/,
+	""
+);
